efda569bd7690e92f05c97166c1d43f18e5f3841
ankitsrivastava777 <ankit@excellencetechnologies.info>
2020-06-29 17:14:50 +0530

    widget created with text field.

---
+++ Excellence/WidgetColorPicker/Block/Widget/Textcolorpicker.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace Excellence\WidgetColorPicker\Block\Widget;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class Textcolorpicker extends Template implements BlockInterface
+{
+
+    protected $_template = "widget/textcolorpicker.phtml";
+}
+
